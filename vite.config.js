29e5f68c65da90fde94106f722026a6907119784
Yaddalapalli-Charan-Kumar-Naidu <charankumarnaidu2004@gmail.com>
2024-10-19 17:59:10 +0530

    First commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/Yaddalapalli-Charan-Kumar-Naidu/TextUtils.git",
+  base: "/TextUtils/",
   plugins: [react()],
 })
